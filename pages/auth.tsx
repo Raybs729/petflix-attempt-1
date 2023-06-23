0374d713147a2705661480c977843b27dce7bd4f
dev global <raybriones729@gmail.com>
2023-06-22 22:44:40 -0500

    first commit. attempted netflix clone

---
+++ pages/auth.tsx
@@ -0,0 +1,17 @@
+
+
+const Auth = () => {
+    return (
+        <div className= "relative h-full w-full bg-[url('/images/hero.jpg')]">
+            <div className = "bg-black w-full h-full lg:bg-opacity-50">
+
+                
+            </div>
+
+        
+        </div>
+
+    );
+}
+
+export default Auth;
